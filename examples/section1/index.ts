54a842ca9d908a52d46ea2dda6b06c945213ab6b
JW9506 <cf.dota@gmail.com>
2019-10-18 00:35:33 -0400

    m

--- examples/section1/index.ts
+++ examples/section1/index.ts
@@ -21,7 +21,7 @@ console.log();
 
 let x: [string, number];
 x = ["Hello", 15];
-console.log(x[0].slice(null, 2));
+console.log(x[0].slice(0, 2));
 console.log(x[1].toPrecision(5));
 
 enum Color {
@@ -35,3 +35,41 @@ console.log(c);
 console.log();
 let colorName: string = Color[Color.Blue];
 console.log(colorName);
+
+let notSure: any = 4;
+notSure = "string";
+notSure = true;
+console.log(notSure);
+
+// Auto type
+let typeGuess = 5;
+
+let list3: any[] = [1, true, "free"];
+function warnUser(): void {
+  console.log("Warn");
+  return;
+}
+
+// 联合类型
+let numNull: number | null = 3;
+numNull = null;
+console.log(numNull);
+
+// never使用场景:
+// while(true)
+// throw Error();
+function error(message: string): never {
+  throw new Error(message);
+}
+
+// error("failed....");
+
+declare function create(o: object | null): void;
+
+let someValue: any = "this is a string";
+let stringLength1: number = (<string>someValue).length;
+let stringLength2: number = (someValue as string).length;
+let stringLength3: number = someValue.length;
+console.log("stringLength", stringLength1);
+console.log("stringLength", stringLength2);
+console.log("stringLength", stringLength3);
--- out/index.js
+++ out/index.js
@@ -1,32 +1,101 @@
-var isDone;
-isDone = true;
-console.log(isDone);
-var decLit = 20;
-var hexLit = 0x14;
-var binLit = 20;
-console.log(hexLit);
-console.log(binLit);
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+function printLabel(labelledObj) {
+    console.log(labelledObj.label);
+}
+var myObj = { size: 10, label: "Size 10 Object" };
+printLabel(myObj);
 console.log();
 console.log();
-var list = [1, 2, 3];
-console.log(list);
-var list2 = [1, 2, 3];
-console.log(list2);
 console.log();
 console.log();
-var x;
-x = ["Hello", 15];
-console.log(x[0].slice(null, 2));
-console.log(x[1].toPrecision(5));
-var Color;
-(function (Color) {
-    Color[Color["Red"] = 0] = "Red";
-    Color[Color["Green"] = 1] = "Green";
-    Color[Color["Blue"] = 2] = "Blue";
-})(Color || (Color = {}));
-var c = Color.Blue;
-console.log(c);
+function createSquare(config) {
+    var newSquare = { color: "white", area: 100 };
+    if (config.color) {
+        newSquare.color = config.color;
+    }
+    if (config.width) {
+        newSquare.area = config.width * config.width;
+    }
+    return newSquare;
+}
+var config = { color: "black", width: 22 };
+var mySquare = createSquare(config);
+console.log(mySquare);
 console.log();
-var colorName = Color[Color.Blue];
-console.log(colorName);
+console.log();
+console.log();
+var p1 = { x: 10, y: 20 };
+console.log(p1);
+var a = [1, 2, 3, 4];
+var ro = a;
+a[0] = 99;
+console.log(ro);
+var mySearch = function (src, sub) {
+    return src.search(sub) > -1;
+};
+console.log(mySearch("abc", "aa"));
+var myArray = ["bob", "fred"];
+console.log(myArray[1]);
+var Animal = /** @class */ (function () {
+    function Animal() {
+    }
+    return Animal;
+}());
+var Dog = /** @class */ (function (_super) {
+    __extends(Dog, _super);
+    function Dog() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    return Dog;
+}(Animal));
+// interface notOkay {
+//   [x: number]: Animal;
+//   [x: string]: Dog;
+// }
+console.log();
+console.log();
+console.log();
+var Clock = /** @class */ (function () {
+    function Clock(h, m) {
+    }
+    Clock.prototype.setTime = function (d) {
+        this.currentTime = d;
+    };
+    return Clock;
+}());
+function createClock(ctor, hour, minute) {
+    return new ctor(hour, minute);
+}
+var DigitalClock = /** @class */ (function () {
+    function DigitalClock(h, m) {
+    }
+    DigitalClock.prototype.tick = function () {
+        console.log("beep beep");
+    };
+    return DigitalClock;
+}());
+var AnalogClock = /** @class */ (function () {
+    function AnalogClock(h, m) {
+    }
+    AnalogClock.prototype.tick = function () {
+        console.log("tick toc");
+    };
+    return AnalogClock;
+}());
+var digital = createClock(DigitalClock, 6, 30);
+digital.tick();
+var Analog = createClock(AnalogClock, 6, 30);
+Analog.tick();
 //# sourceMappingURL=index.js.map
